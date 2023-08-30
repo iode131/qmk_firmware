6a098e16ae70bce86fe6c2ed46fc7011e2b4eafd
Maxime Gendebien <max@Maximes-MBP.lan>
2023-08-30 14:31:32 +0200

    fixed DANCE_0

--- keyboards/splitkb/aurora/sweep/keymaps/iode131/combos.c
+++ keyboards/splitkb/aurora/sweep/keymaps/iode131/combos.c
@@ -1,3 +1,4 @@
+#include QMK_KEYBOARD_H
 
 const uint16_t PROGMEM combo0[] = { MT(MOD_RSFT, KC_N), MT(MOD_RGUI, KC_E), MT(MOD_RCTL, KC_I), MT(MOD_RALT, KC_O), COMBO_END};
 const uint16_t PROGMEM combo1[] = { MT(MOD_LSFT, KC_T), MT(MOD_RSFT, KC_N), COMBO_END};
